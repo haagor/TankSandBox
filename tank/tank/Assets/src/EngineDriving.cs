a82400db2bc3fcc52b42ecfee9966c88faf6f603
haag <sim.paris06@gmail.com>
2020-04-30 21:23:14 +0200

    init with some hours of work

---
+++ tank/tank/Assets/src/EngineDriving.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineDriving : MonoBehaviour
+{
+    public AudioSource engineRun;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) ||
+            Input.GetKeyDown(KeyCode.UpArrow) ||
+            Input.GetKeyDown(KeyCode.LeftArrow) ||
+            Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (!engineRun.isPlaying)
+            {
+                engineRun.Play();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow) &&
+            Input.GetKeyUp(KeyCode.UpArrow) &&
+            Input.GetKeyUp(KeyCode.LeftArrow) &&
+            Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            engineRun.Stop();
+        }
+    }
+}
