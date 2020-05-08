f6f1b9aba18ad3f5e2985c1cea5334dea75de1e7
haag <sim.paris06@gmail.com>
2020-05-08 12:44:08 +0200

    add other Unity Project focus on wheel collider learning

---
+++ tank/tank/Assets/src/TankMovement.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    // public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
+    public float m_Speed = 12f;                 // How fast the tank moves forward and back.
+    public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+    public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
+    public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
+    public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
+    public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+    public float m_MaxRay = 0.1f;                // Distance to ground where tank will be "Grounded"
+    public bool m_isGrounded = true;            // True/False, is tank on the ground?
+
+
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;             // The current value of the turn input.
+    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
+
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnEnable()
+    {
+        // When the tank is turned on, make sure it's not kinematic.
+        m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable()
+    {
+        // When the tank is turned off, set it to kinematic so it stops moving.
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        // The axes names are based on player number.
+        // m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        // m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+        // Store the original pitch of the audio source.
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+
+    private void Update()
+    {
+        // Store the value of both input axes.
+        m_MovementInputValue = Input.GetAxis("Vertical");
+        m_TurnInputValue = Input.GetAxis("Horizontal");
+
+        EngineAudio();
+    }
+
+
+    private void EngineAudio()
+    {
+        // If there is no input (the tank is stationary)...
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
+        {
+            // ... and if the audio source is currently playing the driving clip...
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                // ... change the clip to idling and play it.
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+        else
+        {
+            // Otherwise if the tank is moving and if the idling clip is currently playing...
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                // ... change the clip to driving and play.
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Send ray from bottom of tank.
+        Vector3 m_Ray = transform.TransformDirection(Vector3.down);
+        float m_RayLength = m_Ray.magnitude;
+
+        if (m_RayLength < m_MaxRay)
+        {
+            m_isGrounded = false;
+
+        }
+
+        else if (m_RayLength > m_MaxRay)
+        {
+            m_isGrounded = true;
+        }
+
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        Move();
+        Turn();
+    }
+
+
+    private void Move()
+    {
+        if (m_isGrounded == true)
+        {
+            // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+            Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+            // Apply this movement to the rigidbody's position.
+            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+        }
+
+        else if (m_isGrounded == false)
+        {
+            Vector3 movement = transform.forward * 0;
+            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+        }
+    }
+
+
+    private void Turn()
+    {
+        if (m_isGrounded == true)
+        {
+            // Determine the number of degrees to be turned based on the input, speed and time between frames.
+            float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+            // Make this into a rotation in the y axis.
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+            // Apply this rotation to the rigidbody's rotation.
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        }
+
+        else if (m_isGrounded == false)
+        {
+            Quaternion turnRotation = Quaternion.Euler(0f, 0f, 0f);
+
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        }
+    }
+}
