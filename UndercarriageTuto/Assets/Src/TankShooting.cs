6da69ef9925c36ee7c77d8d414c251e1ccb30c35
haag <sim.paris06@gmail.com>
2020-05-11 21:59:47 +0200

    NICE SHOT !

---
+++ UndercarriageTuto/Assets/Src/TankShooting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;              // Used to identify the different players.
+    public Rigidbody m_Shell;                   // Prefab of the shell.
+    public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public float m_LaunchForce = 30f;           // The force given to the shell if the fire button is not held.
+
+
+    private void Start()
+    {
+        // The fire axis is based on the player number.
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Fire();
+        }
+    }
+
+
+    private void Fire()
+    {
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_LaunchForce * m_FireTransform.forward;
+    }
+}
