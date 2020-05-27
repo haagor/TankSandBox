dda9ae5f7fcd6ccfacc91e9607cbf481a4bdf94a
haag <sim.paris06@gmail.com>
2020-05-27 23:08:24 +0200

    bug instant shell collid

---
+++ UndercarriageTuto/Assets/Src/DisplayShellMarker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayShellMarker : MonoBehaviour
+{
+    private Transform redArrow;
+
+    void Start() {
+        Debug.Log("Y A QUELQU UN?");
+        redArrow = transform.Find("red_arrow");
+        redArrow.GetComponent<Renderer>().enabled = false;
+    }
+
+    void OnCollisionEnter ()
+    {
+        Debug.Log("ça collisionne");
+        redArrow.GetComponent<Renderer>().enabled = true;
+    }
+}
