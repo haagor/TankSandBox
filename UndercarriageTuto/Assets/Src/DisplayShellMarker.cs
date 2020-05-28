ed7e9e0017d6f79397a6d52b2ac346eb2223372f
haag <sim.paris06@gmail.com>
2020-05-28 22:23:39 +0200

    shell marker collision appear on collide

--- UndercarriageTuto/Assets/Src/DisplayShellMarker.cs
+++ UndercarriageTuto/Assets/Src/DisplayShellMarker.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class DisplayShellMarker : MonoBehaviour
 {
     private Transform redArrow;
+    private bool redArrowDisplay = false;
 
     void Start() {
-        Debug.Log("Y A QUELQU UN?");
         redArrow = transform.Find("red_arrow");
         redArrow.GetComponent<Renderer>().enabled = false;
     }
 
-    void OnCollisionEnter ()
-    {
-        Debug.Log("ça collisionne");
+    void OnCollisionEnter () {
+        if (!redArrowDisplay) {
         redArrow.GetComponent<Renderer>().enabled = true;
+            StartCoroutine( collision() );
+        }
+    }
+    
+    private IEnumerator collision() {
+        redArrowDisplay = true;
+        yield return new WaitForSeconds( 0.1f );
+        redArrow.GetComponent<Renderer>().enabled = false;
+        redArrowDisplay = false;
     }
 }
