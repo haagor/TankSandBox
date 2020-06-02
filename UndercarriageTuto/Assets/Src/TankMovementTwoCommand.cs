18bd47e26328e14a5e61e4dc3e254668681b7ef5
haag <sim.paris06@gmail.com>
2020-06-02 15:52:49 +0200

    bug rotate visual wheel

--- UndercarriageTuto/Assets/Src/TankMovementTwoCommand.cs
+++ UndercarriageTuto/Assets/Src/TankMovementTwoCommand.cs
@@ -17,29 +17,46 @@ public class TankMovementTwoCommand : MonoBehaviour
     public void FixedUpdate()
     {
         float motorLeft = maxMotorTorque * Input.GetAxis("TrackLeft");
+        float delta = Time.fixedDeltaTime;
+        float rpmLeft = WheelTrackAlignment(trackLeft);
         foreach (WheelCollider wc in trackLeft.colliders)
         {
             wc.motorTorque = motorLeft;
-            ApplyLocalPositionToVisuals(wc);
+            ApplyLocalPositionToVisuals(wc, rpmLeft, delta);
         }
 
         float motorRight = maxMotorTorque * Input.GetAxis("TrackRight");
+        float rpmRight = WheelTrackAlignment(trackRight);
         foreach (WheelCollider wc in trackRight.colliders)
         {
             wc.motorTorque = motorRight;
-            ApplyLocalPositionToVisuals(wc);
+            ApplyLocalPositionToVisuals(wc, rpmRight, delta);
         }
     }
 
-    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    public void ApplyLocalPositionToVisuals(WheelCollider collider, float rpm, float delta)
     {
         Transform visualWheel = collider.transform.GetChild(0);
 
         Vector3 position;
         Quaternion rotation;
         collider.GetWorldPose(out position, out rotation);
+        rpm = Mathf.Repeat(visualWheel.rotation.y + delta * rpm * 360.0f / 60.0f, 360.0f);
 
         visualWheel.transform.position = position;
-        visualWheel.transform.rotation = rotation;
+        visualWheel.transform.localRotation = Quaternion.Euler(rpm, 0, 0);
+    }
+
+    public float WheelTrackAlignment(Track track) {
+        float rpm = 0.0f;
+        int length = 0;
+        foreach (WheelCollider wc in track.colliders) {
+            if (wc.isGrounded) {
+                rpm += wc.rpm;
+                length += 1;
+            }
+        }
+        rpm /= length;
+        return rpm;
     }
 }
