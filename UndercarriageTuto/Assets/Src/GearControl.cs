940268cc7e0a5cd43c8c20dbb8b462a59d3843f8
haag <sim.paris06@gmail.com>
2020-06-16 19:35:31 +0200

    two gears scene

--- UndercarriageTuto/Assets/Src/GearControl.cs
+++ UndercarriageTuto/Assets/Src/GearControl.cs
@@ -7,8 +7,7 @@ public class GearControl : MonoBehaviour
 
     public Rigidbody gearAxe;
     private float z = 0.0f;
-    private float y = 0.0f;
-
+    
     void Update()
     {
         if (Input.GetKey("up"))
@@ -19,14 +18,6 @@ public class GearControl : MonoBehaviour
         {
             z -= 1.0f;
         }
-        if (Input.GetKey("z"))
-        {
-            y += 1.0f;
-        }
-        if (Input.GetKey("s"))
-        {
-            y -= 1.0f;
-        }
-        gearAxe.MoveRotation(Quaternion.Euler(0,y,z));
+        gearAxe.MoveRotation(Quaternion.Euler(0,0,z));
     }
 }
