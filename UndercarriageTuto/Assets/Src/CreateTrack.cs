d1b2a22baefc1ef0f29418c7c0ad5784e36fa630
haag <sim.paris06@gmail.com>
2020-06-21 16:16:42 +0200

    instantiate at position ><

--- UndercarriageTuto/Assets/Src/CreateTrack.cs
+++ UndercarriageTuto/Assets/Src/CreateTrack.cs
@@ -12,10 +12,12 @@ public class CreateTrack : MonoBehaviour {
         linkClone = Instantiate(link);
         links[0] = linkClone;
         for (int i = 1; i < links.Length; i++) {
-            linkClone = Instantiate(link);
-            linkClone.transform.TransformPoint(new Vector3(2*i,0,0));
+            linkClone = Instantiate(link, new Vector3(2*i,0,0), Quaternion.identity);
             linkClone.AddComponent<HingeJoint>();
             linkClone.GetComponent<HingeJoint>().connectedBody=links[i-1].GetComponent<Rigidbody>();
+            linkClone.GetComponent<HingeJoint>().autoConfigureConnectedAnchor = false;
+            linkClone.GetComponent<HingeJoint>().connectedAnchor = new Vector3(2,0,0);
+            linkClone.GetComponent<HingeJoint>().axis = new Vector3(0,0,1);
             links[i] = linkClone;
         }
     }
